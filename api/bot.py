b2e01fa19c758f57dd14631fe2e094bf4e9c4b6d
illvart <illvartv@gmail.com>
2021-12-11 17:19:49 +0700

    update

--- api/bot.py
+++ api/bot.py
@@ -9,13 +9,8 @@ app = Sanic(__name__)
 
 @app.route("/api/bot", strict_slashes=False)
 async def _bot(request):
-    return response.text("This endpoint is meant for bot and telegram communication.")
-
-
-# TODO: It's working perfectly or not?
-# Register bot as Serverless
-bot_register(True)
-
+    response.text("This endpoint is meant for bot and telegram communication.")
+    return bot_register(True)
 
 if __name__ == "__main__":
     app.run(debug=True, auto_reload=True, host="0.0.0.0", port=3000)
